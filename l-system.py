a678c2f8fb8616c620b21076df312e54a3f437cf
Jeremy Berchtold <i-yam-jeremy@users.noreply.github.com>
2020-09-03 19:20:09 -0700

    Cleaned up script

--- l-system.py
+++ l-system.py
@@ -5,17 +5,28 @@ from sd.api.sdvaluecolorrgba import SDValueColorRGBA
 
 GRAYSCALE_CONVERT = "sbs::compositing::grayscaleconversion"
 UNIFORM_COLOR = "sbs::compositing::uniform"
+TRANSFORM = "sbs::compositing::transformation"
 
-ctx = sd.getContext()
-app = ctx.getSDApplication()
-uiMgr = app.getQtForPythonUIMgr()
-g = uiMgr.getCurrentGraph()
-blank = g.newNode(UNIFORM_COLOR)
-gc = g.newNode(GRAYSCALE_CONVERT)
-gc.setPosition(float2(150, 0))
-blankOutput = blank.getProperties(SDPropertyCategory.Output)[0]
-color = blank.getProperties(SDPropertyCategory.Input)[7]
-print(color.getType())
-blank.setPropertyValue(color, SDValueColorRGBA.sNew(ColorRGBA(1, 1, 1, 1)))
-gcInput = gc.getProperties(SDPropertyCategory.Input)[9]
-print(blank.newPropertyConnection(blankOutput, gc, gcInput))
+def get_graph():
+	ctx = sd.getContext()
+	app = ctx.getSDApplication()
+	uiMgr = app.getQtForPythonUIMgr()
+	g = uiMgr.getCurrentGraph()
+	return g
+
+def create_white_grayscale(g):
+	blank = g.newNode(UNIFORM_COLOR)
+	gc = g.newNode(GRAYSCALE_CONVERT)
+	gc.setPosition(float2(150, 0))
+	blankOutput = blank.getProperties(SDPropertyCategory.Output)[0]
+	color = blank.getProperties(SDPropertyCategory.Input)[7]
+	blank.setPropertyValue(color, SDValueColorRGBA.sNew(ColorRGBA(1, 1, 1, 1)))
+	gcInput = gc.getProperties(SDPropertyCategory.Input)[9]
+	blank.newPropertyConnection(blankOutput, gc, gcInput)
+	return gc
+
+def main():
+	g = get_graph()
+	w = create_white_grayscale(g)
+	
+main()
